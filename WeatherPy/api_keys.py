6043c0ffbd51e0af020cb20f5470f62afe544d67
Madison <madisonoutland@gmail.com>
2024-10-31 16:53:03 -0700

    push first notebook

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "00fec3fb17bb77a07f0d5f416cd0e1cd"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "607bcf29690d45809c8660d6ab03d8c3"
